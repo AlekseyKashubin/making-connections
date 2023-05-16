f6aec314a45a26bb3f6b982e9c74374c4ca8d0ae
AlekseyKashubin <Akashubin@yahoo.com>
2023-05-16 16:16:18 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function removeMe(element){
+    element.remove();
+
+}
+
+function nameChange(element){
+    if(element.innerText == "Jane Doe"){
+        element.innerText = "Aleksey Kashubin";
+    }
+    // else{
+    //     element.innerText = "Jane Doe";
+    //  }
+}
